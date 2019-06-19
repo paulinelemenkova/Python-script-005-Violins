84b1c637830d423e45f6f0e2b1a2f332f81165f5
Pauline Lemenkova <pauline.lemenkova@gmail.com>
2019-06-19 19:47:29 +0800

    Add files via upload

--- Script-005-Violins.py
+++ Script-005-Violins.py
@@ -1,19 +1,28 @@
-#!/usr/bin/env python
-# coding: utf-8
-#
-import seaborn as sb
-from matplotlib import pyplot as plt
-import pandas as pd
 import os
-#
+import pandas as pd
+from matplotlib import pyplot as plt
+import seaborn as sb
+
+sb.set_style('darkgrid')
+sb.set_context('paper')
+
+# loading data
 os.chdir('/Users/pauline/Documents/Python')
 dfM = pd.read_csv("Tab-Bathy.csv")
 dfM.head(5)
-#
-sb.set_style('darkgrid')
-sb.catplot(data=dfM, kind="violin", scale='count', palette="Paired", 
-          orient="v", legend=True, legend_out=True, margin_titles=True, linewidth=0.2,)
+
+# plotting
+sb.catplot(
+           data=dfM, kind="violin", scale='count', palette="Paired",
+           orient="v", legend=True, legend_out=True,
+           margin_titles=True, linewidth=0.2
+           )
 plt.xticks(rotation=45)
-plt.title('Violin plots for the Mariana Trench bathymetry', fontsize=12, fontfamily='serif')
+plt.title('Violin plots for the Mariana Trench bathymetry',
+          fontsize=12, fontfamily='serif')
 plt.subplots_adjust(bottom=0.15,top=0.85)
+
+# visualizing
+plt.tight_layout()
+plt.savefig('plot_Hist.png', dpi=300)
 plt.show()
