b46f1422331798f1374b40118212d9620a95100f
Pauline Lemenkova <pauline.lemenkova@gmail.com>
2019-06-19 19:52:20 +0800

    Add files via upload

--- Script-005-Violins.py
+++ Script-005-Violins.py
@@ -14,14 +14,14 @@ dfM = pd.read_csv("Tab-Bathy.csv")
 dfM.head(5)
 
 # plotting
-sb.catplot(
-           data=dfM, kind="violin", scale='count', palette="Paired",
+sb.catplot(data=dfM, kind="violin", scale='count', palette="Paired",
            orient="v", legend=True, legend_out=True,
            margin_titles=True, linewidth=0.2
            )
 plt.xticks(rotation=45)
 plt.title('Violin plots for the Mariana Trench bathymetry',
-          fontsize=12, fontfamily='serif')
+          fontsize=12, fontfamily='serif'
+          )
 plt.subplots_adjust(bottom=0.15,top=0.85)
 
 # visualizing
